1e33899ab921f0d84c9f36395acb765380e5f54c
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-02-22 22:06:28 +0900

    Time: 82 ms (98.20%), Space: 44.5 MB (61.80%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -6,17 +6,17 @@
 var findContentChildren = function(g, s) {
     // g - children
     // s - cookies
-    g.sort((a, b) => a - b); // sort g in non-decreasing order
-  s.sort((a, b) => a - b); // sort s in non-decreasing order
-  let i = 0; // pointer for g
-  let j = 0; // pointer for s
-  let content = 0; // counter for content children
-  while (i < g.length && j < s.length) {
-    if (s[j] >= g[i]) { // if s[j] can satisfy g[i], increment content and move to next child
-      content++;
-      i++;
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+    let i=0;
+    let j=0;
+    let content = 0;
+    while(i<g.length && j<s.length){
+        if(s[j]>=g[i]){
+            content++;
+            i++
+        }
+        j++
     }
-    j++; // move to next cookie regardless
-  }
-  return content;
+    return content;
 };
