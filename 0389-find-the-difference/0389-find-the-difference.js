cff108cd84204c8e2777e0755e7442050a3a781f
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-02-22 21:40:23 +0900

    Time: 101 ms (17.73%), Space: 46.3 MB (13.30%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -14,6 +14,8 @@ var findTheDifference = function(s, t) {
     // tList를 돌면서 sList와 비교
     // 
     for(let i=0;i<tList.length;i++){
-        if(i===sList.length||tList[i]!==sList[i]) return tList[i];
+        if(i===sList.length||tList[i]!==sList[i]) return tList[i]; //추가될(?) 요소를 return
     }
 };
+
+console.log(findTheDifference('abcd','abcde'))
