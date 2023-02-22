f221dca3b72461233cb8f229a2d8aa099fedb954
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-02-22 21:51:59 +0900

    Time: 63 ms (88.43%), Space: 44.2 MB (42.33%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    // 주어진 배열에서 세번째로 큰 숫자 고르기
+    // let maxNum = 0;
+    // for(let i=0;i<nums.length;i++){
+    //     if(nums[i+1]<nums[i]) {
+    //         maxNum = nums[i+1];
+    //     }
+    // }
+    // return maxNum;
+    nums.sort((a,b)=>b-a);
+    const uniqueNums = [...new Set(nums)];
+    return uniqueNums.length<3?uniqueNums[0]:uniqueNums[2]; //uniqueNums 길이가 3보다 작으면 맨 첫번째 요소 반환하고 아니면 마지막 요소
+};
