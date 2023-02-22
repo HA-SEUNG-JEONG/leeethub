9ff3bd8a06585418095c0c67e91dd22271f70bfc
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-02-22 22:19:45 +0900

    Time: 101 ms (60.00%), Space: 45.1 MB (35.96%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -3,16 +3,16 @@
  * @param {number[]} s
  * @return {number}
  */
-const findContentChildren=(g, s)=>{
-    // g - children
-    // s - cookies
-    g.sort((a, b) => a - b);
-    s.sort((a, b) => a - b);
-    let a=0;
-    let b=0;
-    let content = 0;
-    while(a<g.length && b<s.length){
-        s[b] >= g[a] ? (a++, b++) : b++;
-    }
-    return a;
+const findContentChildren = (g, s) => {
+  // g - children
+  // s - cookies
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+  let a = 0;
+  let b = 0;
+  let content = 0;
+  while (a < g.length && b < s.length) {
+    s[b] >= g[a] ? (a++, b++) : b++;
+  }
+  return a;
 };
