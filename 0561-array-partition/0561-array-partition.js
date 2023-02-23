391acc4ba5c44eda3ec0993428cb7024bc3bf221
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-02-23 11:15:40 +0900

    Time: 123 ms (77.61%), Space: 47.2 MB (60.45%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -2,14 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var arrayPairSum = function(nums) {
+const arrayPairSum = (nums) => {
+  // 2n개의 정수 배열이 주어짐
+  // 모든 (a[i],b[i])의 합이 최대가 되도록 그룹화
+  // 최대화된 합을 return
+  // [1,4,3,2] 면
+  // (1,4) (3,2)로 그룹화 한 다음 각각에 대한 min 값을 구해서 서로 더하기
+  // (1,3) (2,4)로 그룹화 한 다음 각각에 대한 min 값을 구해서 서로 더하기
+  // (1,2) (3,4)로 그룹화 한 다음 각각에 대한 min 값을 구해서 서로 더하기
+
   const numsLen = nums.length;
   const sortedArr = nums.sort((a, b) => a - b);
+  //   console.log(sortedArr); // [ 1, 2, 2, 5, 6, 6 ]
   let sum = 0;
 
-  for (let i = 0; i < numsLen; i += 2) {
-    sum += sortedArr[i];
-  }
+  sortedArr.forEach((sorted, index) => {
+    if (index % 2 === 0) sum += sorted;
+  });
 
   return sum;
 };
