46eb4b0b4df0767bc8249451b4ec740d1490d93e
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-02-26 09:45:32 +0900

    Time: 81 ms (77.92%), Space: 46.2 MB (58.36%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -3,7 +3,7 @@
  * @return {number[]}
  */
 const findErrorNums=(nums)=>{
-//누락된 숫자와 중복된 숫자 정보가 필요함
+  //누락된 숫자와 중복된 숫자 정보가 필요함
   // 빈 해시 테이블 생성
   // 중복된 변수, 누락된 변수를 각각 -1로 설정
   // 해시 테이블에 nums의 원소가 없으면 1(있는 값으로 설정)로 변경
@@ -15,11 +15,16 @@ const findErrorNums=(nums)=>{
   let duplicate = 0,
     missing = 0;
 
+  // 주어진 입력 배열로 테스트
+  // freq에 num이 들어있지 않으면 freq[num]을 1로 설정
+  // 들어있으면 duplicate, 즉 중복 숫자가 들어있는 것으로 판단하여 duplicate를 1로 넣기
   nums.forEach((num) => {
     if (!freq[num]) freq[num] = 1;
     else duplicate = num;
   });
 
+  //nums를 돌면서 1부터 시작(문제에서 1부터 n까지)
+  // freq[i]가 없으면 missing을 해당 인덱스로 재할당하고 break
   for (let i = 1; i <= n; i++) {
     if (!freq[i]) {
       missing = i;
