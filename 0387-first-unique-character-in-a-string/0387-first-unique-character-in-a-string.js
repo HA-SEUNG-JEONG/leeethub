78915f0ce76d2d74c644d7bd0f6e1307fde4e710
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-03-02 23:52:30 +0900

    Time: 124 ms (37.68%), Space: 48.6 MB (21.23%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -4,8 +4,9 @@
  */
 const firstUniqChar = (s) => {
   // 처음으로 반복되지 않는 문자열을 찾고, 그에 대한 인덱스를 반환, 없으면 -1
-let result = -1;
-  s.split("").forEach((char, index) => {
+
+  let result = -1;
+  Array.from(s).forEach((char, index) => {
     if (s.indexOf(char) === s.lastIndexOf(char) && result === -1) {
       result = index;
     }
