b2d8eb578609735bb05e6edb53972d7266e703d5
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-03-05 23:24:27 +0900

    Time: 60 ms (89.91%), Space: 41.6 MB (98.94%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.js
+++ 0682-baseball-game/0682-baseball-game.js
@@ -2,37 +2,57 @@
  * @param {string[]} operations
  * @return {number}
  */
-const calPoints = (operations) => {
   // operations[i]는 record에 들어가야 하는 i번째 operation
   // + : 이전 2개의 점수 합
   // D : 이전 점수의 2배
   // C : 유효하지 않은 이전 점수, record에서 제거
+const calPoints = (operations) => {
   const result = [];
+  
+  const getScore = (operation) => {
+    const score = Number(operation);
+    result.push(score);
+    return score;
+  };
+  
+  const doubleLastScore = () => {
+    const last = result[result.length - 1];
+    const doubled = last * 2;
+    result.push(doubled);
+    return doubled;
+  };
+  
+  const sumLastTwoScores = () => {
+    const lastTwoResult = result.slice(-2);
+    const [lastResult1, lastResult2] = lastTwoResult;
+    const added = lastResult1 + lastResult2;
+    result.push(added);
+    return added;
+  };
+  
+  const removeLastScore = () => {
+    const removed = result.pop();
+    return -removed;
+  };
+  
   let sum = 0;
+  
   operations.forEach((operation) => {
     switch (operation) {
       case "C":
-        sum -= result.pop();
+        sum += removeLastScore();
         break;
       case "D":
-        const last = result[result.length - 1];
-        const doubled = last * 2;
-        result.push(doubled);
-        sum += doubled;
+        sum += doubleLastScore();
         break;
       case "+":
-        const lastTwoResult = result.slice(-2);
-        const [lastResult1, lastResult2] = lastTwoResult;
-        const added = lastResult1 + lastResult2;
-        result.push(added);
-        sum += added;
+        sum += sumLastTwoScores();
         break;
       default:
-        const score = Number(operation);
-        result.push(score);
-        sum += score;
+        sum += getScore(operation);
         break;
     }
   });
+  
   return sum;
 };
