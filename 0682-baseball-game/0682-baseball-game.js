501da98d0311fe830683058c704d248f62be5745
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-03-05 14:35:40 +0900

    Time: 57 ms (94.29%), Space: 42.4 MB (66.00%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.js
+++ 0682-baseball-game/0682-baseball-game.js
@@ -9,26 +9,30 @@ const calPoints = (operations) => {
   // C : 유효하지 않은 이전 점수, record에서 제거
   const result = [];
   let sum = 0;
-  for (let i = 0; i < operations.length; i++) {
-    if (!isNaN(operations[i])) {
-      result.push(Number(operations[i]));
-      sum += Number(operations[i]); //sum에 더해줘야 함
-    } else if (operations[i] === "C") {
-      const removed = result.pop(); // pop해서 변수에 할당 필요
-      sum -= removed;
-    } else if (operations[i] === "D") {
-      const lastElement = result[result.length - 1];
-      // pop이 아니라 가장 마지막 요소 ,pop을 쓰면 기존 배열에서 제거되기 때문에 이전 배열을 유지하도록 해야함
-      const doubled = 2 * lastElement;
-      result.push(doubled);
-      sum += doubled;
-    } else if (operations[i] === "+") {
-      const first = result[result.length - 1];
-      const last = result[result.length - 2];
-      const added = first + last;
-      result.push(added);
-      sum += added;
+  operations.forEach((operation) => {
+    switch (operation) {
+      case "C":
+        sum -= result.pop();
+        break;
+      case "D":
+        const last = result[result.length - 1];
+        const doubled = last * 2;
+        result.push(doubled);
+        sum += doubled;
+        break;
+      case "+":
+        const lastTwoResult = result.slice(-2);
+        const [lastResult1, lastResult2] = lastTwoResult;
+        const added = lastResult1 + lastResult2;
+        result.push(added);
+        sum += added;
+        break;
+      default:
+        const score = Number(operation);
+        result.push(score);
+        sum += score;
+        break;
     }
-  }
+  });
   return sum;
 };
