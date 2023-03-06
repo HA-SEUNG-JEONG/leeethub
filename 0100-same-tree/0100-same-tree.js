00ead26ec976784ae4d90d056d4ff171981f240e
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-03-06 10:15:58 +0900

    Time: 56 ms (90.01%), Space: 42.1 MB (77.49%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,8 +12,7 @@
  * @return {boolean}
  */
 const isSameTree = (p, q) => {
-if (!p && !q) return true; // 두 노드가 모두 null인 경우
-  if (!p || !q) return false; // 두 노드 중 하나만 null인 경우
-  if (p.val !== q.val) return false; // 노드의 값이 다른 경우
+  if (!p && !q) return true; // 두 노드가 모두 null인 경우
+  if (!p || !q || p.val !== q.val) return false;
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right); // 좌우 하위 트리를 재귀적으로 비교
 };
