42e0128fe3731a3bf125e070a293a4b38e8b32c5
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-03-06 10:51:59 +0900

    Time: 72 ms (63.51%), Space: 45 MB (13.47%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -12,12 +12,11 @@
  */
 const isSymmetric = (root) => {
   const isSymmetricTree = (p, q) => {
-    if (!p && !q) return true; // 두 노드가 모두 null인 경우
-    if (!p || !q) return false; // 두 노드 중 하나만 null인 경우
-    if (p.val !== q.val) return false; // 노드의 값이 다른 경우
+    if (!root || (!p && !q)) return true; // 루트 노드가 null이거나 root가 없는 경우
+
+    if (!p || !q || p.val !== q.val) return false; // 두 노드 중 하나만 null인 경우
     return isSymmetricTree(p.left, q.right) && isSymmetricTree(p.right, q.left); // 좌우 하위 트리를 재귀적으로 비교
   };
 
-  if (!root) return true; // 루트 노드가 null인 경우
   return isSymmetricTree(root.left, root.right); // 좌우 하위 트리를 재귀적으로 비교
 };
