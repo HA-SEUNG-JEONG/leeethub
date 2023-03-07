41fac140a6b7aadd6d22f7fe70c81fcb61e06822
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-03-07 09:48:38 +0900

    Time: 253 ms (57.42%), Space: 81.1 MB (95.83%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const minDepth = (root) => {
+  // 최소 깊이 찾아내기
+  if (root == null) return 0;
+  if (root.left == null && root.right == null) return 1;
+  if (root.left == null) return minDepth(root.right) + 1;
+  if (root.right == null) return minDepth(root.left) + 1;
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+};
