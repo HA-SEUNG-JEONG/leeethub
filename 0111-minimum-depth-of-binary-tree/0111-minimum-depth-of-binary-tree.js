c3f6723160c7231d7dc21739153b1b935e3aea0c
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-03-07 09:52:06 +0900

    Time: 256 ms (49.94%), Space: 80.8 MB (96.94%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -13,7 +13,7 @@
 const minDepth = (root) => {
   // 최소 깊이 찾아내기
   if (root == null) return 0;
-  if (root.left == null) return minDepth(root.right) + 1;
   if (root.right == null) return minDepth(root.left) + 1;
+  if (root.left == null) return minDepth(root.right) + 1;
   return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
 };
