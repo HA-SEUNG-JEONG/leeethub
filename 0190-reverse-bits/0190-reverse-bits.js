cefc20aa08101006944c2277b55bfc1165a0ac69
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-03-08 10:54:04 +0900

    Time: 63 ms (90.22%), Space: 44.2 MB (28.94%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.js
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -3,7 +3,6 @@
  * @return {number} - a positive integer
  */
 const reverseBits = (n) => {
-  console.log(n.toString(2));
   const binaryStr = n.toString(2).padStart(32, "0");
   const reversedBinaryStr = binaryStr.split("").reverse().join("");
   return parseInt(reversedBinaryStr, 2);
