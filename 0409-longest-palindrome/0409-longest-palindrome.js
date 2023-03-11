09aa5489e0a96037485842206d9eb531b8dc014f
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-03-11 12:12:54 +0900

    Time: 68 ms (69.80%), Space: 44.6 MB (18.43%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -6,20 +6,12 @@ const longestPalindrome = (s) => {
   //소문자 또는 대문자로 구성된 문자열이 주어지면 해당 문자로 작성할 수 있는 가장 긴 회문의 길이를 반환
   // 문자는 대소문자를 구분하는데,
   // 예를 들어, "Aa"는 여기서 회문으로 간주되지 않는다.
-  const countMap = new Map();
-  let odd = 0;
-  for (let i = 0; i < s.length; i++) {
-    const char = s.charAt(i);
-    countMap.set(char, (countMap.get(char) || 0) + 1);
+  let ans = 0;
+  const keys = {};
+
+  for (let char of s) {
+    keys[char] = (keys[char] || 0) + 1;
+    if (keys[char] % 2 == 0) ans += 2;
   }
-  //   console.log(countMap); // { 'a' => 1, 'b' => 1, 'c' => 4, 'd' => 2 }
-  let len = 0;
-  for (const item of countMap.values()) {
-    if (item % 2 === 0) len += item;
-    else {
-      len += item - 1;
-      odd = 1; // 회문 돌 때 한번은 필요함
-    }
-  }
-  return len + odd;
+  return s.length > ans ? ans + 1 : ans;
 };
